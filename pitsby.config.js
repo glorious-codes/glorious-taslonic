f74924e2463b572763367e2b4e74db867905db1f
Rafael Camargo <hello@rafaelcamargo.com>
2019-06-02 23:36:17 -0300

    Add pitsby to project

---
+++ pitsby.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  projects: [
+    {
+      engine: "vanilla",
+      collectDocsFrom: "./src"
+    }
+  ],
+  styles: [
+    "./dist/taslonic.css"
+  ],
+  scripts: [
+    "./dist/taslonic.js"
+  ],
+  outputDirectory: "./docs"
+}

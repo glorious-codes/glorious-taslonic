d037e1960abb8e1eab3f24f53e2dcd76db56161b
Rafael Camargo <hello@rafaelcamargo.com>
2020-12-06 22:15:11 -0300

    feat: create select

---
+++ src/base/services/select/select.test.js
@@ -0,0 +1,13 @@
+import selectService from './select';
+
+describe('Select Service', () => {
+  it('should wrapper not contain modifier css classes by default', () => {
+    const cssClasses = selectService.buildWrapperCssClasses();
+    expect(cssClasses).toEqual('t-select-wrapper');
+  });
+
+  it('should wrapper append disabled modifier css class if it has been given as true', () => {
+    const cssClasses = selectService.buildWrapperCssClasses({ disabled: true });
+    expect(cssClasses).toEqual('t-select-wrapper t-select-wrapper-disabled');
+  });
+});

23cccfcfac05bda274faf9e81ba901a23629ed33
Rafael Camargo <hello@rafaelcamargo.com>
2021-01-29 18:13:08 -0300

    fix: add missing react bundle to the lib

--- pre-publish.sh
+++ pre-publish.sh
@@ -3,5 +3,6 @@
 npm run build:clear
 NODE_ENV=production npm run build
 cp ./dist/react.js ./react.js
+cp ./dist/react-styled.js ./react-styled.js
 cp ./dist/vue-plugin.js ./vue-plugin.js
 cp ./dist/taslonic.css ./taslonic.css

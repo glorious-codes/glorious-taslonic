e9d2d16b9a688f7557b9cf56a76bbb8fe477b442
Rafael Camargo <hello@rafaelcamargo.com>
2022-12-20 16:46:46 -0300

    chore: refactor textarea test suite

--- src/react/components/textarea/textarea.test.js
+++ src/react/components/textarea/textarea.test.js
@@ -1,104 +1,28 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { FormControl } from '@react/components/form-control/form-control';
-import { Textarea } from './textarea';
+import { run } from '@base/tests/textarea';
+import { customRender, screen } from '@react/services/testing/testing';
+import { Textarea } from '@react/';
 
-describe('Textarea', () => {
-  function mount(props = {}){
-    return shallow(
-      <Textarea
-        cols={ props.cols }
-        rows={ props.rows }
-        placeholder={ props.placeholder }
-        value={ props.value }
-        validations={ props.validations }
-        block={ props.block }
-        disabled={ props.disabled }
-        required={ props.required }
-        { ...props }
-      >
-        { props.content }
-      </Textarea>
-    );
-  }
+function mount({
+  value,
+  placeholder,
+  validations,
+  block,
+  disabled,
+  required,
+  ...rest
+} = {}){
+  return customRender(
+    <Textarea
+      value={value}
+      placeholder={placeholder}
+      validations={validations}
+      block={block}
+      disabled={disabled}
+      required={required}
+      {...rest}
+    />
+  );
+}
 
-  it('should pass value to form control', () => {
-    const value = 'Taslonic';
-    const wrapper = mount({ value });
-    expect(wrapper.find(FormControl).prop('value')).toEqual(value);
-  });
-
-  it('should pass required to form control', () => {
-    const required = true;
-    const wrapper = mount({ required });
-    expect(wrapper.find(FormControl).prop('required')).toEqual(required);
-  });
-
-  it('should pass validations to form control', () => {
-    const validations = [];
-    const wrapper = mount({ validations });
-    expect(wrapper.find(FormControl).prop('validations')).toEqual(validations);
-  });
-
-  it('should pass block to form control', () => {
-    const block = true;
-    const wrapper = mount({ block });
-    expect(wrapper.find(FormControl).prop('block')).toEqual(block);
-  });
-
-  it('should form control query for a textarea', () => {
-    const wrapper = mount();
-    expect(wrapper.find(FormControl).prop('formControlElSelector')).toEqual('textarea');
-  });
-
-  it('should not be disabled by default', () => {
-    const wrapper = mount();
-    expect(wrapper.find('textarea').prop('disabled')).not.toBeDefined();
-  });
-
-  it('should optionally set select as disabled', () => {
-    const wrapper = mount({ disabled: true });
-    expect(wrapper.find('textarea').prop('disabled')).toEqual(true);
-  });
-
-  it('should optionally autofocus textarea', () => {
-    const wrapper = mount({ autofocus: true });
-    expect(wrapper.find('textarea').prop('autofocus')).toEqual(true);
-  });
-
-  it('should optionally set a name', () => {
-    const name = 'fruit';
-    const wrapper = mount({ name });
-    expect(wrapper.find('textarea').prop('name')).toEqual(name);
-  });
-
-  it('should optionally set visible columns', () => {
-    const cols = '15';
-    const wrapper = mount({ cols });
-    expect(wrapper.find('textarea').prop('cols')).toEqual(cols);
-  });
-
-  it('should optionally set visible rows', () => {
-    const rows = '8';
-    const wrapper = mount({ rows });
-    expect(wrapper.find('textarea').prop('rows')).toEqual(rows);
-  });
-
-  it('should not show a placeholder by default', () => {
-    const wrapper = mount();
-    expect(wrapper.find('textarea').prop('placeholder')).toEqual(undefined);
-  });
-
-  it('should optionally set a placeholder', () => {
-    const placeholder = 'Type anything...';
-    const wrapper = mount({ placeholder });
-    expect(wrapper.find('textarea').prop('placeholder')).toEqual(placeholder);
-  });
-
-  it('should optionally set select listeners', () => {
-    const onBlur = jest.fn();
-    const wrapper = mount({ onBlur });
-    wrapper.find('textarea').simulate('blur');
-    expect(onBlur).toHaveBeenCalled();
-  });
-});
+run(mount, { screen });
--- src/vue/components/textarea/textarea.test.js
+++ src/vue/components/textarea/textarea.test.js
@@ -1,96 +1,42 @@
-import { shallowMount } from '@vue/test-utils';
-import { tFormControl } from '@vue/components/form-control/form-control';
-import { tTextarea } from './textarea';
+import { run } from '@base/tests/textarea';
+import { customRender, screen, stringifyAttributes } from '@vue/services/testing/testing';
+import { tTextarea } from '@vue/';
 
-describe('Select', () => {
-  function mount({ listeners = {}, ...propsData } = {}){
-    return shallowMount(tTextarea, { propsData, listeners });
-  }
-
-  it('should pass value to form control', () => {
-    const value = 'Taslonic';
-    const wrapper = mount({ value });
-    expect(wrapper.findComponent(tFormControl).props('value')).toEqual(value);
+function mount({
+  value,
+  placeholder,
+  validations,
+  readonly,
+  block,
+  required,
+  disabled,
+  ...rest
+} = {}){
+  return customRender({
+    components: { tTextarea },
+    data(){
+      return {
+        value,
+        placeholder,
+        validations,
+        readonly,
+        block,
+        required,
+        disabled
+      };
+    },
+    template: `
+      <t-textarea
+        v-model="value"
+        :placeholder="placeholder"
+        :validations="validations"
+        :readonly="readonly"
+        :block="block"
+        :required="required"
+        :disabled="disabled"
+        ${stringifyAttributes(rest)}
+      />`
   });
+}
 
-  it('should pass required to form control', () => {
-    const required = true;
-    const wrapper = mount({ required });
-    expect(wrapper.findComponent(tFormControl).props('required')).toEqual(required);
-  });
-
-  it('should pass validations to form control', () => {
-    const validations = [];
-    const wrapper = mount({ validations });
-    expect(wrapper.findComponent(tFormControl).props('validations')).toEqual(validations);
-  });
-
-  it('should pass block to form control', () => {
-    const block = true;
-    const wrapper = mount({ block });
-    expect(wrapper.findComponent(tFormControl).props('block')).toEqual(block);
-  });
-
-  it('should form control query for a textarea', () => {
-    const wrapper = mount();
-    expect(wrapper.findComponent(tFormControl).props('formControlElSelector')).toEqual('textarea');
-  });
-
-  it('should not be disabled by default', () => {
-    const wrapper = mount();
-    expect(wrapper.find('textarea').attributes('disabled')).not.toBeDefined();
-  });
-
-  it('should optionally set select as disabled', () => {
-    const wrapper = mount({ disabled: true });
-    expect(wrapper.find('textarea').attributes('disabled')).toEqual('disabled');
-  });
-
-  it('should optionally autofocus textarea', () => {
-    const wrapper = mount({ autofocus: true });
-    expect(wrapper.find('textarea').attributes('autofocus')).toEqual('autofocus');
-  });
-
-  it('should optionally set a name', () => {
-    const name = 'fruit';
-    const wrapper = mount({ name });
-    expect(wrapper.find('textarea').attributes('name')).toEqual(name);
-  });
-
-  it('should optionally set visible columns', () => {
-    const cols = '15';
-    const wrapper = mount({ cols });
-    expect(wrapper.find('textarea').attributes('cols')).toEqual(cols);
-  });
-
-  it('should optionally set visible rows', () => {
-    const rows = '8';
-    const wrapper = mount({ rows });
-    expect(wrapper.find('textarea').attributes('rows')).toEqual(rows);
-  });
-
-  it('should not show a placeholder by default', () => {
-    const wrapper = mount();
-    expect(wrapper.find('textarea').attributes('placeholder')).toEqual(undefined);
-  });
-
-  it('should optionally set a placeholder', () => {
-    const placeholder = 'Type anything...';
-    const wrapper = mount({ placeholder });
-    expect(wrapper.find('textarea').attributes('placeholder')).toEqual(placeholder);
-  });
-
-  it('should optionally set select listeners', () => {
-    const onBlur = jest.fn();
-    const wrapper = mount({ listeners: { blur: onBlur } });
-    wrapper.find('textarea').trigger('blur');
-    expect(onBlur).toHaveBeenCalled();
-  });
-
-  it('should optionally set a custom attribute', () => {
-    const label = 'Notes';
-    const wrapper = mount({ 'aria-label': label });
-    expect(wrapper.findComponent(tFormControl).attributes('aria-label')).toEqual(undefined);
-    expect(wrapper.find('textarea').attributes('aria-label')).toEqual(label);
-  });
-});
+run(mount, { screen });

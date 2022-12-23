ea14bd0f2f4e510ee3f100e95d946b29143c876b
Rafael Camargo <hello@rafaelcamargo.com>
2022-12-23 17:10:38 -0300

    chore: improve vue content service

--- src/vue/services/content/content.js
+++ src/vue/services/content/content.js
@@ -1,10 +1,29 @@
 import componentBuilder from '@vue/builders/component/component';
 
-export const renderContent = (content, containerElement) => {
-  const element = typeof content == 'object' ? buildComponent(content) : content;
-  containerElement.prepend(element);
+export const renderContent = (content = '', containerElement) => {
+  const render = getRendererByContentType(typeof content);
+  render(content, containerElement);
 };
 
-function buildComponent({ template, ...rest }){
-  return componentBuilder.build({ controller: rest, template }).element;
+function getRendererByContentType(type){
+  return {
+    'object': renderVueComponent,
+    'string': renderStringContent
+  }[type];
+}
+
+function renderStringContent(content, containerElement){
+  const childNodes = parseStringContent(content);
+  Array.from(childNodes).reverse().forEach(node => containerElement.prepend(node));
+}
+
+function parseStringContent(content){
+  const rootEl = document.createElement('div');
+  rootEl.innerHTML = content;
+  return rootEl.childNodes;
+}
+
+function renderVueComponent({ template, ...rest }, containerElement){
+  const { element } = componentBuilder.build({ controller: rest, template });
+  containerElement.prepend(element);
 }

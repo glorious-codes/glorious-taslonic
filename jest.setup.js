1d4fbbf8983f91eaf7dabad075860786b2b08c8d
Rafael Camargo <hello@rafaelcamargo.com>
2022-12-23 19:23:45 -0300

    chore: remove enzyme and @vue/test-utils

--- jest.setup.js
+++ jest.setup.js
@@ -1,8 +1,5 @@
 import '@testing-library/jest-dom'
 import Vue from '@vue';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 
 Vue.config.productionTip = false;
 Vue.config.devtools = false;
-Enzyme.configure({ adapter: new Adapter() });
